7db276d9baf3b4eb082c9f3bb10dc7a4a848cbb4
unknown <calmez0609@gmail>
2023-12-11 19:44:25 +0800

    Initial commit

---
+++ AutoPush.bat
@@ -0,0 +1,7 @@
+git init
+git remote add origin https://github.com/calmez0609/PickUpSystem.git
+git add .
+git commit -m "Initial commit"
+git config --global user.email calmez0609@gmail
+git push -u origin master
+pause
